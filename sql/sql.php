39658466c3c8f165f76645181ddaa2e829b8834c
root <root@richie-HP-EliteBook-820-G1>
2019-05-22 12:38:31 +0300

    changes on sql script

--- sql/sql.php
+++ sql/sql.php
@@ -3,7 +3,7 @@
 require('../pages/database/db.php');
 
 $sql1="CREATE TABLE `agents` (
-    `id` int(11) NOT NULL,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
     `first_name` text NOT NULL,
     `last_name` text NOT NULL,
     `Gender` text NOT NULL,
@@ -14,7 +14,8 @@ $sql1="CREATE TABLE `agents` (
     `date_created` text NOT NULL,
     `custom_date` text NOT NULL,
     `delete_status` int(11) NOT NULL,
-    `reset_status` text NOT NULL
+    `reset_status` text NOT NULL,
+    PRIMARY KEY (`id`)
   ) ENGINE=MyISAM DEFAULT CHARSET=latin1;";
   
   $sql1_query=mysqli_query($con,$sql1,$sql11);
@@ -31,10 +32,11 @@ $sql1="CREATE TABLE `agents` (
   
   
   $sql2="CREATE TABLE `matatu` (
-    `id` int(11) NOT NULL,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
     `plate_no` text NOT NULL,
     `pic` text NOT NULL,
-    `date` text NOT NULL
+    `date` text NOT NULL,
+    PRIMARY KEY (`id`)
   ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
   ";
   
@@ -52,7 +54,7 @@ $sql1="CREATE TABLE `agents` (
   
   
   $sql3="CREATE TABLE `stakeholders` (
-    `id` int(11) NOT NULL,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
     `first_name` text NOT NULL,
     `last_name` text NOT NULL,
     `rank` text NOT NULL,
@@ -63,7 +65,8 @@ $sql1="CREATE TABLE `agents` (
     `date_created` text NOT NULL,
     `custom_date` text NOT NULL,
     `delete_status` int(11) NOT NULL,
-    `reset_status` int(11) NOT NULL
+    `reset_status` int(11) NOT NULL,
+    PRIMARY KEY (`id`)
   ) ENGINE=MyISAM DEFAULT CHARSET=latin1;";
   
   
@@ -80,7 +83,10 @@ $sql1="CREATE TABLE `agents` (
   
   $sql4="CREATE TABLE `matatu_ownership` (
     `matatu_id` int(11) NOT NULL,
-    `stakeholder_id` int(11) NOT NULL
+    `stakeholder_id` int(11) NOT NULL,
+
+    FOREIGN KEY (`matatu_id`) REFERENCES `matatu`(id),
+  FOREIGN KEY (`stakeholder_id`) REFERENCES `stakeholders`(id)
   ) ENGINE=MyISAM DEFAULT CHARSET=latin1;";
   
   
@@ -102,7 +108,9 @@ $sql1="CREATE TABLE `agents` (
     `amount` int(11) NOT NULL,
     `agent_id` int(11) NOT NULL,
     `date_of_payment` text NOT NULL,
-    `timestamp` text NOT NULL
+    `timestamp` text NOT NULL,
+    FOREIGN KEY (`agent_id`) REFERENCES `agents`(id),
+  FOREIGN KEY (`matatu_id`) REFERENCES `matatu`(id)
   ) ENGINE=MyISAM DEFAULT CHARSET=latin1;";
   
   
@@ -121,7 +129,7 @@ $sql1="CREATE TABLE `agents` (
   
   
   $sql6="CREATE TABLE `sacco-employee` (
-    `id` int(11) NOT NULL,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
     `first_name` text NOT NULL,
     `last_name` text NOT NULL,
     `Gender` text NOT NULL,
@@ -129,7 +137,8 @@ $sql1="CREATE TABLE `agents` (
     `email` text NOT NULL,
     `password` text NOT NULL,
     `date_created` text NOT NULL,
-    `custom_date` text NOT NULL
+    `custom_date` text NOT NULL,
+    PRIMARY KEY (`id`)
   ) ENGINE=MyISAM DEFAULT CHARSET=latin1;";
   
   
@@ -142,11 +151,14 @@ $sql1="CREATE TABLE `agents` (
   else{
       echo "Error creating table sacco employee</br>";
   }
+
+
   $sql7="CREATE TABLE `superuser` (
-    `id` int(11) NOT NULL,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
     `name` text NOT NULL,
     `email` text NOT NULL,
-    `password` text NOT NULL
+    `password` text NOT NULL,
+    PRIMARY KEY (`id`)
   ) ENGINE=MyISAM DEFAULT CHARSET=latin1;";
   
   $sql7_query=mysqli_query($con,$sql7);
@@ -161,7 +173,7 @@ $sql1="CREATE TABLE `agents` (
  }
 
  $sql77="INSERT INTO `superuser`(`id`, `name`, `email`, `password`) 
-VALUES ('2','admin', 'admin@info.co.ke','1234567890') ";
+VALUES ('1','admin', 'admin@info.co.ke','1234567890') ";
  $sql77_query=mysqli_query($con,$sql77);
 
  if($sql77_query){
